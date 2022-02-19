a4eab8280f2b7a8f2da1c8202f18238328e0357c
Halwoll <97443146+Halwoll@users.noreply.github.com>
2022-02-19 17:26:57 +0000

    Adding more information

--- directory.rb
+++ directory.rb
@@ -1,28 +1,29 @@
-student_count = 12
+student_count = 11
 
 students = [
-    "Dr Hannibal Lecter",
-    "Darth Vader",
-    "Nurse Ratchet",
-    "Michael Corleon",
-    "Alex DeLarge",
-    "The Wicked Witch of the West",
-    "Terminator",
-    "Freddie Krueger",
-    "The Joker",
-    "Joffrey Baratheon",
-    "Norman Bates",
-    "Baal"
+    [name: "Dr Hannibal Lecter", cohort: :november],
+    [name: "Darth Vader", cohort: :november],
+    [name: "Nurse Ratchet", cohort: :november],
+    [name: "Michael Corleon", cohort: :november],
+    [name: "Alex DeLarge", cohort: :november],
+    [name: "The Wicked Witch of the West", cohort: :november],
+    [name: "Terminator", cohort: :november],
+    [name: "Freddie Krueger", cohort: :november],
+    [name: "The Joker", cohort: :november],
+    [name: "Joffrey Baratheon", cohort: :november],
+    [name: "Norman Bates", cohort: :november]
 ]
 def print_header
     puts "The students of VILLANS academy"
     puts "-------------"
 end
+
 def print(students)
     students.each do |student|
-        puts student
+      puts "#{student[:name]} (#{student[:cohort]} cohort)"
     end
-end
+  end
+  
 def print_footer(students)
-    print "Overall, we have #{student.count} great students"
+    puts "Overall, we have #{student.count} great students"
 end
